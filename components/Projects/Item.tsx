d7cd260ec2d38336765dc42352e91c554c84ddfc
leejinw <leejinw@nxtcorp.co.kr>
2023-04-02 00:38:28 +0900

    :beetle: fixed #20 Project 페이지 렌더링 시, 아이콘 일부 렌더링 안되는 버그 수정 완료

--- components/Projects/Item.tsx
+++ components/Projects/Item.tsx
@@ -6,14 +6,6 @@ import { styled } from '@mui/material/styles';
 import ModuleCard from 'modules/Card';
 import theme from '../../config/theme';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'dark',
-  // ...theme?.typography?.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  minHeight: '100%',
-}));
-
 const moveToLink = (href: string) => {
   window.open(href, '_blank');
 };
