7b9b1642cd75fd86a5d7dd17362f05691684d464
JinWoo_Lee <dlwlsdn201@naver.com>
2023-08-20 17:19:28 +0900

    :beetle: project UI 클릭 시, 현재 창에서 링크 이동되는 버그 수정

--- components/Projects/Item.tsx
+++ components/Projects/Item.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import { Grid, Paper } from '@mui/material';
 import { entryType } from 'types/projects';
-import { styled } from '@mui/material/styles';
 import ModuleCard from 'modules/Card';
 import theme from '../../config/theme';
 
 const moveToLink = (href: string) => {
-  window.open(href, '_blank');
+  window.open(href);
 };
 
 const ProjectItem = (data: entryType, index: number) => {
   return (
-    <a key={data.id} href={data.href} className="group">
+    <a key={data.id} className="group">
       <ModuleCard
         data={data}
         onClick={() => {
